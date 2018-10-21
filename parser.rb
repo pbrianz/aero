bf4d2b4498c1fbb9f03f2787fb8020088bea0bfe
Zack <36819949+pbrianz@users.noreply.github.com>
2018-10-21 11:41:52 -0600

    wip

--- parser.rb
+++ parser.rb
@@ -1,3 +1,29 @@
 require_relative "importer"
 require_relative "loader"
 
+imported = {}
+
+def parse(filename)
+  data = loader(filename)
+  str = ""
+  if data["imports"]
+    for import in data["imports"] do
+      imported[import] = import
+    end
+  else
+    print("No imports defined.  Skipping.")
+  end
+  for key in data.keys do
+    type = "Class"
+    if imported[data[key]] then
+      importer(data[key],File.open(filename + ".aero"))
+    else
+      k = data[key]
+      if k.Type then
+        if k.Type.downcase == "class" then
+          type = "Class"
+        end
+      end
+    end
+  end
+end
